e2e4b40840203c0576aa25157babcf0f17091458
Sowmya Eemani <sowmyaeemani@Sowmyas-MBP.lan>
2024-09-21 20:03:14 -0400

    update: level 3,4,5

--- Bandit/level3.sh
+++ Bandit/level3.sh
@@ -1,11 +1,11 @@
 # level 3 --> level 4
 # The password for the next level is stored in a hidden file in the inhere directory.
 
+#username: bandit3@bandit.labs.overthewire.org
+#password: MNk8KNH3Usiio41PRUEoDFPqfxLPlSmx
+
 # ls - lists all the files/directories 
 # ll - short for ls -l, lists all the files/direcotries including the hidden ones.
-
-#username: bandit3@bandit.labs.overthewire.org
-#password: 2WmrDFRmJIq3IPxneAaMGhap0pFhF3NJ
 
 cd inhere
 ll
