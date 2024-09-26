e7d7d37b7b91c1afab6129abab0c1daf9df9a60e
Sowmya Eemani <sowmyaeemani@Sowmyas-MacBook-Pro.local>
2024-09-26 17:33:09 -0400

    update: levels 16,17

--- Bandit/level14.sh
+++ Bandit/level14.sh
@@ -1,10 +1,12 @@
-# level 13 --> level 14
-# The password for the next level is stored in /etc/bandit_pass/bandit14 and can only be read by user bandit14. \n
-# For this level, you don’t get the next password, but you get a private SSH key that can be used to log into the \n
-# next level. Note: localhost is a hostname that refers to the machine you are working on
+# level 14 --> level 15
+# The password for the next level can be retrieved by submitting the password of the current level to port \n
+# 30000 on localhost.
 
-#username: bandit13@bandit.labs.overthewire.org
-#password: MU4VWeTyJk8ROof1qqmcBPaLh7lDCPvS
+#username: bandit14@bandit.labs.overthewire.org
+#password: MU4VWeTyJk8ROof1qqmcBPaLh7lDCPvS (output from level 13)
+echo "MU4VWeTyJk8ROof1qqmcBPaLh7lDCPvS" | nc localhost 30000
+# echo: send the data(string) as an argument to the next part
+# nc: short for Netcat which is a netwokr utility used for reading from and writing to network connections via TCP/UDP
 
 # copy the password to ssh into your next bandit level
 # use the "exit" command to exit bandit1
